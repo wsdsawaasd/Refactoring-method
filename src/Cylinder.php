15a7ee485c362b824aff330c644446dbfee71fc5
Phan Van Luan <phanvanluan611996@gmail.com>
2018-10-26 23:09:59 +0700

    first commit

---
+++ src/Cylinder.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: phanluan
+ * Date: 26/10/2018
+ * Time: 22:50
+ */
+class Cylinder
+{
+    public function getVolume($radius, $height)
+    {
+        $baseArea = pi() * $height * $radius;
+        $perimeter = 2 * pi() * $radius;
+        $volume = $perimeter * $height + 2 * $baseArea;
+        return $volume;
+    }
+}
